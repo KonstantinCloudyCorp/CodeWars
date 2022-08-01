5d0398893e90c96af632d2064040dc69e265de4a
KonstantinCloudyCorp <Konstantin92Borodin@gmail.com>
2022-08-01 13:25:27 +0300

    Более верное решение

--- NumberOfDecimalDigits.js
+++ NumberOfDecimalDigits.js
@@ -1,8 +1,3 @@
 function digits(n) {
-    let arr = String(n).split('');
-    let amount = 0;
-    for(i=0; i< arr.length; i++){
-        amount+=1
-    }
-    return amount
+    return String(n).length
 }
